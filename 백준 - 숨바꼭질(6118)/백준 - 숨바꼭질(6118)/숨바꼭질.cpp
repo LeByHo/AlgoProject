4a1b05c7ab517830256acc7c91f157eb1c7afed8
LeByHo <dlqudgh0731@naver.com>
2018-08-07 09:42:58 +0900

    백준 - 숨바꼭질(6118)

---
+++ 백준 - 숨바꼭질(6118)/백준 - 숨바꼭질(6118)/숨바꼭질.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int n_ = 20000 + 2;
+
+int n, m, a, b, c, dst[n_];
+vector<int> gph[n_];
+
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i = 0, u, v; i < m; i++) {
+		scanf("%d %d", &u, &v);
+		gph[u].push_back(v);
+		gph[v].push_back(u);
+	}
+
+	queue<int> que;
+	que.push(1);
+	while (!que.empty()) {
+		int now = que.front();
+		que.pop();
+		for (int nxt : gph[now]) if (!dst[nxt]) {
+			que.push(nxt);
+			dst[nxt] = dst[now] + 1;
+			b = max(b, dst[nxt]);
+		}
+	}
+
+	for (int i = 2; i <= n; i++) if (dst[i] == b) {
+		if (!a) a = i; c++;
+	}
+
+	printf("%d %d %d\n", a, b, c);
+
+	return 0;
+}
