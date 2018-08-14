3069a65d4a359fc13f07ea707c481a980aed82b4
LeByHo <dlqudgh0731@naver.com>
2018-08-14 09:43:45 +0900

    백준 - 타노스(15802)

---
+++ 백준 - 타노스(15802)/백준 - 타노스(15802)/타노스.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+//타노스가 당신 앞에 나타났다. 
+//당신은 살아남을 수 있을까?
+#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	string me;
+	cin >> me;
+	cout << "0" << endl;;
+	return 0;
+}
