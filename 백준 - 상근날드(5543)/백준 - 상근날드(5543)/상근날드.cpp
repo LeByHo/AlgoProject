00a8d8124b870fb054d1abd0409815ebbe33ee55
LeByHo <dlqudgh0731@naver.com>
2018-08-16 09:52:44 +0900

    백준 - 상근날드(5543)

---
+++ 백준 - 상근날드(5543)/백준 - 상근날드(5543)/상근날드.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int arr[5], small = 2001, small2 = 2001;
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> arr[i];
+		if (arr[i] < small)
+		{
+			small = arr[i];
+		}
+	}
+	for (int i = 3; i <= 4; i++)
+	{
+		cin >> arr[i];
+		if (arr[i] < small2)
+		{
+			small2 = arr[i];
+		}
+	}
+	cout << small + small2 - 50 << '\n';
+}
