08a0cabcbf07fb328306cf4038b4caa93c5b06fe
LeByHo <dlqudgh0731@naver.com>
2018-08-23 10:50:08 +0900

    백준 - 1로 만들기(1463)

---
+++ 백준 - 1로 만들기(1463)/백준 -1로 만들기(1463)/1로만들기.cpp
@@ -0,0 +1,24 @@
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int Dp[1000001];
+int main(void) {
+
+	int N;
+	scanf("%d", &N);
+
+	Dp[1] = 0;
+
+	for (int i = 2; i <= N; i++)
+	{
+		Dp[i] = Dp[i - 1] + 1;
+		if (i % 2 == 0)
+			Dp[i] = min(Dp[i], Dp[i / 2] + 1);
+		if (i % 3 == 0)
+			Dp[i] = min(Dp[i], Dp[i / 3] + 1);
+	}
+
+	printf("%d\n", Dp[N]);
+	return 0;
+}
